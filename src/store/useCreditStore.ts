01a451d3164e3e0a143507ddbf7e04f9f5cdae51
Bhavya Jain <jainbhavya359@gmail.com>
2026-02-05 22:43:54 +0530

    Emi fix

--- src/store/useCreditStore.ts
+++ src/store/useCreditStore.ts
@@ -1,25 +1,21 @@
-import { set } from "mongoose";
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
 type CreditState = {
   score: number;
   index: number;
   show: boolean;
+
   setScore: (score: number) => void;
   setIndex: (index: number) => void;
-  showResult: () => void;
+  showResult: (value: boolean) => void;
 };
 
-export const useCreditStore = create<CreditState>()(persist(
-  (set) => ({
-    score: 0,
-    index: 0,
-    show: false,
+export const useCreditStore = create<CreditState>((set) => ({
+  score: 0,
+  index: 0,
+  show: false,
 
-    setScore: (score) => set({ score }),
-    setIndex: (index) => set({ index }),
-    showResult: () => set({ show: true }),
-  }),
-  {name: "credit-store"}
-));
+  setScore: (score) => set({ score }),
+  setIndex: (index) => set({ index }),
+  showResult: (value) => set({ show: value }),
+}));
