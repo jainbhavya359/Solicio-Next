22f701867f120f07222569471a9af956a715da06
Bhavya Jain <jainbhavya359@gmail.com>
2026-02-18 20:24:32 +0530

    inventory page cache bug

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,9 +1,12 @@
 import Inventory from "@/src/components/Inventory";
+import { Suspense } from "react";
 
-export default function InventoryPage(){
+export default function InventoryPage() {
     return (
         <div className="bg-white text-slate-900 antialiased">
-            <Inventory />
+            <Suspense fallback={<div className="min-h-screen flex items-center justify-center font-bold text-slate-400">Loading Inventory...</div>}>
+                <Inventory />
+            </Suspense>
         </div>
     )
 }
